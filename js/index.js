62804f94838e61a49016cc3bcaa4855f396f04fe
Jeff Lyon <jeff@albatrossdigital.com>
2013-04-26 12:50:50 -0700

    Initial import

---
+++ js/index.js
@@ -0,0 +1,23 @@
+var app = {
+    initialize: function() {
+        this.bind();
+    },
+    bind: function() {
+        document.addEventListener('deviceready', this.deviceready, false);
+    },
+    deviceready: function() {
+        // note that this is an event handler so the scope is that of the event
+        // so we need to call app.report(), and not this.report()
+        app.report('deviceready');
+        //window.location="http://jeff.home.albatrossdemos.com";
+        window.location="http://jeff.work.albatrossdemos.com/new";
+    },
+    report: function(id) { 
+        console.log("report:" + id);
+        // hide the .pending <p> and show the .complete <p>
+        document.querySelector('#' + id + ' .pending').className += ' hide';
+        var completeElem = document.querySelector('#' + id + ' .complete');
+        completeElem.className = completeElem.className.split('hide').join('');
+    }
+
+};
