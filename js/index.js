15c5c09d47346b32c6b8b1e6261a3495f5c6cb5a
Jeff Lyon <jeff@albatrossdigital.com>
2013-04-30 22:35:16 -0700

    Fixing loading bg of main app

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ var app = {
     deviceready: function() {
         // note that this is an event handler so the scope is that of the event
         // so we need to call app.report(), and not this.report()
-        window.location = 'http://staging.imgfave.com'; 
+        window.location = 'http://staging.imgfave.com/albatrossdigital'; 
     },
     report: function(id) { 
         /*
